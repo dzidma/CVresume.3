fafe3378c9ad36523989d495b2ac9db2bff2d632
dzidma <73446042+dzidma@users.noreply.github.com>
2020-12-17 12:11:12 -1100

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,20 @@
+function GithubLink(){
+    url = "https://github.com/dzidma";
+    window.open(
+        url,
+        "_blank"
+    );
+}
+
+function InstagramLink(){
+    url = "https://www.instagram.com/dzidma/?hl=hr";
+    window.open(
+        url,
+        "_blank"
+    );
+}
+
+
+
+
+
